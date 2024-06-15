713cf94e14495d6a959a39b6abe92d78607ea541
gnz-e <94329797+gnz-e@users.noreply.github.com>
2024-06-15 13:10:14 -0600

    login and register form

--- app/src/main/java/com/example/eventify/loginActivity.java
+++ app/src/main/java/com/example/eventify/loginActivity.java
@@ -2,13 +2,36 @@ package com.example.eventify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
+
+    public TextView txt_sign_up;
+    public EditText email_input_signin;
+    public EditText password_input_signin;
+    public Button btn_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txt_sign_up = findViewById(R.id.txt_sign_up);
+        email_input_signin = findViewById(R.id.email_input_signin);
+        password_input_signin = findViewById(R.id.password_input_signin);
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        txt_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginActivity.this, registerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/eventify/registerActivity.java
+++ app/src/main/java/com/example/eventify/registerActivity.java
@@ -2,13 +2,40 @@ package com.example.eventify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class registerActivity extends AppCompatActivity {
+
+    public TextView txt_sign_in;
+    public EditText user_input_signup;
+    public EditText email_input_signup;
+    public EditText password_input_signup;
+    public EditText confirm_password_input_signup;
+    public Button btn_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txt_sign_in = findViewById(R.id.txt_sign_in);
+        user_input_signup = findViewById(R.id.user_input_signup);
+        email_input_signup = findViewById(R.id.email_input_signup);
+        password_input_signup = findViewById(R.id.password_input_signup);
+        confirm_password_input_signup = findViewById(R.id.confirm_password_input_signup);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+
+        txt_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registerActivity.this, loginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
