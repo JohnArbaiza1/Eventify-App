6dbdb92558b70e99afaeb40361f99cc5dd9a73a3
gnz-e <94329797+gnz-e@users.noreply.github.com>
2024-06-24 22:48:49 -0600

    adapter y diseno de busqueda general

---
+++ app/src/main/java/com/example/eventify/Adaptador/BuscarAdapter.java
@@ -0,0 +1,84 @@
+package com.example.eventify.Adaptador;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.eventify.Objets.Evento;
+import com.example.eventify.R;
+import com.example.eventify.activities.EventoDetalles;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class BuscarAdapter extends BaseAdapter {
+
+    public Context context;
+    public List<Evento> dataEvento;
+    private List<Evento> dataEventoOriginal;
+
+    public BuscarAdapter(Context context, List<Evento> dataEvento, List<Evento> dataEventoOriginal) {
+        this.context = context;
+        this.dataEvento = dataEvento;
+        this.dataEventoOriginal = dataEventoOriginal;
+    }
+
+    @Override
+    public int getCount() {
+        return dataEvento.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dataEvento.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(context).inflate(R.layout.evento_items,null);
+
+        TextView titulo = convertView.findViewById(R.id.tituloEventos);
+        TextView asistentes = convertView.findViewById(R.id.asistenteEventos);
+        TextView ubicacion = convertView.findViewById(R.id.ubicacionEventos);
+
+        ImageView imagenEventos = convertView.findViewById(R.id.row_eventos);
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent eventosDetalles = new Intent(context, EventoDetalles.class);
+                eventosDetalles.putExtra("id", dataEvento.get(position).getIdEvento().toString());
+                eventosDetalles.putExtra("nombreEvento", dataEvento.get(position).getNombreEvento());
+                eventosDetalles.putExtra("imagenEvento", dataEvento.get(position).getImg());
+                eventosDetalles.putExtra("descripcionEvento", dataEvento.get(position).getDescripciN());
+                eventosDetalles.putExtra("asistenteEvento", String.valueOf(dataEvento.get(position).getAsistentes()));
+                eventosDetalles.putExtra("ubicacionEvento", dataEvento.get(position).getUbicacion());
+                eventosDetalles.putExtra("fechaCreacion", dataEvento.get(position).getFechaCreacion());
+                eventosDetalles.putExtra("fechaDelEvento", dataEvento.get(position).getFecha());
+                eventosDetalles.putExtra("categoriaEvento", dataEvento.get(position).getCategoria());
+                eventosDetalles.putExtra("nombrePersona", dataEvento.get(position).getUsername());
+
+                eventosDetalles.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(eventosDetalles);
+            }
+        });
+
+        titulo.setText(dataEvento.get(position).getNombreEvento());
+        asistentes.setText(String.valueOf(dataEvento.get(position).getAsistentes()));
+        ubicacion.setText(dataEvento.get(position).getUbicacion());
+
+        Picasso.get().load(dataEvento.get(position).getImg()).into(imagenEventos);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/eventify/activities/BuscarActivity.java
+++ app/src/main/java/com/example/eventify/activities/BuscarActivity.java
@@ -2,16 +2,84 @@ package com.example.eventify.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-
+import com.example.eventify.Fragments.DatePickerFragment2;
 import com.example.eventify.R;
 
-public class BuscarActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscarActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    public Spinner spinnerOpciones, spinnerAPI;
+    public EditText txtFecha;
+    public ListView listBusquedaGeneral;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+
+        spinnerOpciones = findViewById(R.id.spinnerBusqueda);
+        spinnerAPI = findViewById(R.id.spinnerAPI);
+        txtFecha = findViewById(R.id.fechasBusqueda);
+        listBusquedaGeneral = findViewById(R.id.listViewBusquedaGeneral);
+
+        List<String> opciones = new ArrayList<>();
+        opciones.add("Categoria");
+        opciones.add("Ubicacion");
+        opciones.add("Fecha");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOpciones.setAdapter(adapter);
+
+        txtFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        spinnerOpciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOption = (String) parent.getItemAtPosition(position);
+                if ("Fecha".equals(selectedOption)) {
+                    txtFecha.setVisibility(View.VISIBLE);
+                } else {
+                    txtFecha.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void showDatePickerDialog() {
+        DialogFragment datePickerFragment = new DatePickerFragment2();
+        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        // Crear una cadena con la fecha seleccionada
+        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+        // Mostrar la fecha seleccionada en el EditText de fecha
+        txtFecha.setText(selectedDate);
     }
 }
